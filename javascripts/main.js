e83e409aa1270ace208f60d658185f577d911add
palmierie <ericmpalmieri@gmail.com>
2017-08-28 10:22:57 -0500

    trying to figure out how to wait for the DOM loader function

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,7 +9,7 @@ let movieAPILoader = require('./api.js'),
     handlebarHelper = require("./hbsHelpers.js");
 
 var movieIDsArray = [];
-var movieObjArray = [];
+
 
 $("#GoMALCOLM").click(function(){ //clicks or presses enter
     // gets value from search
@@ -23,33 +23,39 @@ $("#GoMALCOLM").click(function(){ //clicks or presses enter
             for (let i = 0; i < movieData.results.length; i++) {
                 movieIDsArray.push(movieData.results[i].id);
             }  
-            for (let i = 0; i < movieIDsArray.length; i++) {
-                requestMovieByID(movieIDsArray[i]);
-            }
-        })
-        .then((movieObjArray)=>{
-          console.log('movieOBJArray loads');
-          
-        }); 
+            loopIDthenDOMDisplay(movieIDsArray);
+        });
+
+
+        // .then((movieObjArray)=>{
+        //  loadMoviesToDOM(movieObjArray);
+        //     console.log('movieOBJArray loads');
+        
+        // }); 
 });
 
 // PROMISE.ALL then loadMoviesToDOM
                 
-function requestMovieByID(movieID) {
-movieAPILoader.getMoviesWithCredits(movieID)
-    .then((movieDataWithCredits)=>{
-         console.log('movieDataWithCredits', movieDataWithCredits);
-       movieObjArray.push(movieDataWithCredits);
-    });
-
+var movieObjArray = [];
+function loopIDthenDOMDisplay(idArray) {
+    for (let i = 0; i < idArray.length; i++) {
+        requestMovieByID(idArray[i]);  
+    }
+    console.log('movieOBJArray out function', movieObjArray.length);
+    
+    loadMoviesToDOM(movieObjArray);
 }
-
+function requestMovieByID(movieID) {
+    movieAPILoader.getMoviesWithCredits(movieID)
+        .then((movieDataWithCredits)=>{
+        console.log('movieDataWithCredits', movieDataWithCredits);
+        movieObjArray.push(movieDataWithCredits);
+        }); 
+}
 
 function loadMoviesToDOM(movieData) {
     let card = document.createElement("div");
     card.innerHTML = movieTemplate(movieData);
-    // console.log('load movies to dom called');
-
+     console.log('load movies to dom called', movieData);
     $("#movieDiv").append(card);
-
 }
