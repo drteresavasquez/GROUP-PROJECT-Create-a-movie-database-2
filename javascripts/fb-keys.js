e73b6f30b5914a69d42d3921159365927c0e6bfb
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-10-14 18:50:16 -0500

    stash

---
+++ javascripts/fb-keys.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function getFBKey() {
+ return {
+  //  apiKey: "AIzaSyB_qfhqUeYQaHqPmFeh-_ewd_uHkF2whnI",
+  //  authDomain: "moviehistorydb.firebaseapp.com",
+  //  databaseURL: "https://moviehistorydb.firebaseio.com"
+  apiKey: "AIzaSyDQksCicsOwKh5YbHygziPVCdCeUjfAR4w",
+  authDomain: "movie-65c61.firebaseapp.com",
+  databaseURL: "https://movie-65c61.firebaseio.com",
+  projectId: "movie-65c61",
+  storageBucket: "",
+  messagingSenderId: "742176459343"
+ };
+}
+
+module.exports = getFBKey;
